7c3171fcffe903fe713e11a5c04fad3c8a2eedc7
Kale Leach <kleach1212@gmail.com>
2022-07-28 19:50:15 -0700

    Commit 2; Adding login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+  enable :sessions
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  post '/users' do 
+    user = User.create_with(password: params[:password]).find_or_create_by(username:params[:username])
+    user.to_json
+    session[:user_id] = user.id
   end
+
+  post '/sessions' do
+    user = User.find_by(username:params[username], password: params[password])
+    if user
+      session[:user_id] = user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions'
+  end
+
+  get '/users/home' do
+    user = User.find(session[:user_id])
+  end
+
+  get '/sessions/logout' do
+    sessions.clear
+  end
+
 
 end
