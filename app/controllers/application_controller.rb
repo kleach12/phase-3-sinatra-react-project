5dd02d16c2427ac80de92ffc8c3ec7a6a8cf6a3d
Kale Leach <kleach1212@gmail.com>
2022-07-30 09:04:37 -0700

    Commit 3: Working on login/signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   enable :sessions
   # Add your routes here
-  post '/users' do 
-    user = User.create_with(password: params[:password]).find_or_create_by(username:params[:username])
-    user.to_json
-    session[:user_id] = user.id
+  post '/users/signup' do 
+    @user = User.create_with(password: params[:password]).find_or_create_by(username:params[:username])
+    @user.to_json
+    # session[:user_id] = @user.id
   end
 
-  post '/sessions' do
-    user = User.find_by(username:params[username], password: params[password])
-    if user
-      session[:user_id] = user.id
+  post '/login' do
+    @user = User.find_by(username:params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
       redirect '/users/home'
     end
-    redirect '/sessions'
+    redirect '/login'
+    @user.to_json
   end
 
   get '/users/home' do
-    user = User.find(session[:user_id])
+    @user = User.find(session[:user_id])
+    @user.to_json
   end
 
   get '/sessions/logout' do
     sessions.clear
+  end
+
+  get '/all' do
+    @users = User.all
+    @users.to_json
   end
 
 
